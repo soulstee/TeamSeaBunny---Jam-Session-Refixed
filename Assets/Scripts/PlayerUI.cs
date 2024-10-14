bc1966c233d0de495d0ba12cb17d354215ff1a01
aschahal <aschahal1234@gmail.com>
2024-10-14 17:26:48 -0500

    integrated UI

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class PlayerWinLoss : MonoBehaviour {
+public class PlayerScore : MonoBehaviour {
+
+    public TextMeshProUGUI scoreText;
+
+    public static float score = 0;
+    public static float accuracy;
+    public static float totalScore;
 
     public Text winText;
     public Text lossText;
-
     public GameObject player;
-    // Start is called before the first frame update
-    void Start() {
-        
+
+    public void SetScore(float add){
+        score += add;
+        UpdateScoreText();
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void UpdateScoreText(){
+        scoreText.text = score.ToString();
     }
 
     private void OnTriggerEnter(Collider2D collision) {
